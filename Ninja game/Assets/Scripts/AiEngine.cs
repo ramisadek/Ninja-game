c9ead574a5fcfb3999dae18eb7e8b253979a3352
Eslam El-feky <eslammiro37@gmail.com>
2018-09-11 22:03:35 +0200

    added lookRadius

--- Ninja game/Assets/Scripts/AiEngine.cs
+++ Ninja game/Assets/Scripts/AiEngine.cs
@@ -7,6 +7,12 @@ public class AiEngine : MonoBehaviour
 
     [SerializeField] Vector3 MovmentVector = new Vector3(10f, 10f, -10f);
     [SerializeField] float Period = 2f;
+    // The detection circle
+    [SerializeField] float lookRadius = 2f;
+    // The target we want to detect if entered the lookRadius
+    // TODO: there is another way to assign the target other than having to assign it with unity - will check it later
+    [SerializeField] Transform target;
+
 
     [Range(0, 1)] [SerializeField] float MovmentFactor;
 
@@ -29,5 +35,29 @@ public class AiEngine : MonoBehaviour
         MovmentFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = MovmentVector * MovmentFactor;
         transform.position = startingPos + offset;
+    }
+
+    // using it when playing with physics
+    private void FixedUpdate()
+    {
+        // just checking if the target has entered the lookRadius
+        if (Vector3.Distance(target.position, transform.position) <= lookRadius)
+        {
+            // GameOver();
+        }
+    }
+
+    // TODO : implement GameOver Method
+    // called if the target detected
+    private void GameOver()
+    {
+
+    }
+
+    // just to show tha lookradius (detection raduis) when gizmos is selected
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, lookRadius);
     }
 }
