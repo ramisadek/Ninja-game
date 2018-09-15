a130ea4d6da40ea0313245c0b0aa8eaf11a9db7d
ramisadek <35673698+ramisadek@users.noreply.github.com>
2018-09-15 16:07:53 +0200

    character&layers update
    - removed the box collider in the player to pass double collision error
    - change the layers of the bushes to left bush and just bush to make the character flip when it collide
    - add the flip code to the characterEngine.cs

--- Ninja game/Assets/Scripts/CharacterEngine.cs
+++ Ninja game/Assets/Scripts/CharacterEngine.cs
@@ -28,4 +28,31 @@ public class CharacterEngine : MonoBehaviour
             transform.localPosition = Vector3.Lerp(transform.localPosition, target, Time.deltaTime * speed);
         }*/
     }
+
+
+    /// <summary>
+ 
+    // this function to flip the character its make the scale x = -1 when it collide with left bush to seems...
+    // ..normal in movment.
+
+    /// </summary>
+    /// <param name="collision"></param>
+ 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "LeftBush")
+        {
+            Vector3 Scale = transform.localScale;
+            Scale.x = -1;
+            transform.localScale = Scale;
+        }
+        if (collision.tag == "bushes")
+        {
+            Vector3 Scale = transform.localScale;
+            Scale.x = 1;
+            transform.localScale = Scale;
+        }
+        // not imp TODO: if the player want to go backward the eyes must be in the direction...
+        // ..of the bush as from bush RIght bush 2 to RIght bush 1
+    }
 }
